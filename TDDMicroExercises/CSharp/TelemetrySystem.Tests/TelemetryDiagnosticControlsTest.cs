ca2d11634b186f59fd4ec03e321c8ac9cbd8a536
Chris Bingham <ccbing@gmail.com>
2012-05-22 00:06:19 +0100

    initial, rahter weak passing test

--- TDDMicroExercises/CSharp/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
+++ TDDMicroExercises/CSharp/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
 
 namespace TDDMicroExercises.TelemetrySystem.Tests
 {
@@ -8,6 +9,11 @@ namespace TDDMicroExercises.TelemetrySystem.Tests
         [Test]
         public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
         {
+            var telemetryDiagnosticControls = new TelemetryDiagnosticControls();
+
+            telemetryDiagnosticControls.CheckTransmission();
+
+            Assert.That(telemetryDiagnosticControls.DiagnosticInfo, Is.Not.Empty);
         }
 
     }
