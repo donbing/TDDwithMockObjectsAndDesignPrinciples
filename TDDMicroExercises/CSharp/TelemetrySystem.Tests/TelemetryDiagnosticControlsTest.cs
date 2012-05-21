3ceca891bf5319cb470c5df2781c8d52d98c525f
Chris Bingham <ccbing@gmail.com>
2012-05-22 00:15:04 +0100

    refactor test for TelemeteryDiagnosticControls and Assert send was called on ITelemeteryClient

--- TDDMicroExercises/CSharp/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
+++ TDDMicroExercises/CSharp/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
@@ -7,20 +7,28 @@ namespace TDDMicroExercises.TelemetrySystem.Tests
     [TestFixture]
     public class TelemetryDiagnosticControlsTest
     {
+        private const string ExpectedTelemetryClientResponse = "foo";
+
         [Test]
         public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
         {
             var telemetryClient = MockRepository.GenerateMock<ITelemetryClient>();
 
-            telemetryClient.Stub(client => client.OnlineStatus).Return(true);
-            telemetryClient.Stub(client => client.Receive()).Return("foo");
+            telemetryClient
+                .Stub(client => client.OnlineStatus)
+                .Return(true);
+
+            telemetryClient
+                .Stub(client => client.Receive())
+                .Return(ExpectedTelemetryClientResponse);
             
             var telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClient);
 
             telemetryDiagnosticControls.CheckTransmission();
             
-            Assert.That(telemetryDiagnosticControls.DiagnosticInfo, Is.EqualTo("foo"));
-        }
+            telemetryClient.AssertWasCalled(client => client.Send(TelemetryClient.DiagnosticMessage));
 
+            Assert.That(telemetryDiagnosticControls.DiagnosticInfo, Is.EqualTo(ExpectedTelemetryClientResponse));
+        }
     }
 }
